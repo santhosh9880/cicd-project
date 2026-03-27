4a98eecb5d65448690a236023eb33a6cc6276d88
Ubuntu <ubuntu@ip-172-31-16-203.ec2.internal>
2026-03-27 05:04:37 +0000

    the project setup

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Clean old files
-rm -rf /var/www/html/*
-
-# Copy new files
-cp -r * /var/www/html/
-
-# Set permissions
-chmod -R 755 /var/www/html/
+cp -r /opt/codedeploy-agent/deployment-root/*/deployment-archive/* /var/www/html/
